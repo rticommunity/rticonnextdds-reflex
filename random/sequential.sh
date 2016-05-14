40a6ce432beed195bda7defb68c4de0dc19deabf
Sumant Tambe <sumant@rti.com>
2016-05-14 10:15:08 -0700

    65535 property-tests complete

--- random/sequential.sh
+++ random/sequential.sh
@@ -16,16 +16,16 @@ for i in `seq $begin $end`;
 do
   touch test/reflex_prop_test.cxx 
   
-  make -f make/Makefile.i86Linux-gcc CXX=g++5 CXXSTD=c++14 RANDOM=$i prop_test 
+  make -f make/Makefile.x64Linux-gcc CXX=g++49 CXXSTD=c++14 RANDOM=$i prop_test 
   
   if [ "$?" -ne "0" ]; then 
-    make -f make/Makefile.i86Linux-gcc CXX=g++5 CXXSTD=c++14 RANDOM=$i prop_test 2>&1 > compile_$i.txt
+    make -f make/Makefile.x64Linux-gcc CXX=g++49 CXXSTD=c++14 RANDOM=$i prop_test 2>&1 > compile_$i.txt
     continue
   fi
   
-  objs/i86Linux3gcc4.8.2/reflex_prop_test 1
+  objs/x64Linux2.6gcc4.4.5/reflex_prop_test 1
   
   if [ "$?" -ne "0" ]; then 
-    objs/i86Linux3gcc4.8.2/reflex_prop_test 1 > run_$i.txt
+    objs/x64Linux2.6gcc4.4.5/reflex_prop_test 1 > run_$i.txt
   fi
 done
